b83df5ac345858825d392ecb7b5cd9ec94a30103
dcdara <danhutnhi@gmail.com>
2022-06-19 21:40:43 +0700

    update

---
+++ src/models/Product.js
@@ -0,0 +1,12 @@
+class Product {
+    constructor(tenSP, gia, screen, backCamera, frontCamera, hinhAnh, mota, type) {
+      this.tenSP = tenSP;
+      this.price = price;
+      this.screen = screen;
+      this.backCamera = backCamera;
+      this.frontCamera = frontCamera;
+      this.hinhAnh = hinhAnh;
+      this.mota = mota;
+      this.type = type;
+    }
+  }
